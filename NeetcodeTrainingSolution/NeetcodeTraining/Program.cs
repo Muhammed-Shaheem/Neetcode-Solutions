c4c2c87bfab2c2ca86f2c1a2fc39a21733e0192f
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-09-27 09:22:10 +0530

    Solution of HasDuplicate

---
+++ NeetcodeTrainingSolution/NeetcodeTraining/Program.cs
@@ -0,0 +1,11 @@
+﻿using NeetcodeTraining.Solutions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 4,4};
+        Console.WriteLine(ContainsDuplicate.HasDuplicate(nums));
+
+    }
+}
---
+++ NeetcodeTrainingSolution/NeetcodeTraining/Solutions/ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿namespace NeetcodeTraining.Solutions;
+
+public static class ContainsDuplicate
+{
+    public static bool HasDuplicate(int[] nums)
+    {
+        HashSet<int> ints = new();
+
+        int length = nums.Length;
+        for (int i = 0; i < length; i++)
+        {
+
+           bool contains = ints.Add(nums[i]);
+
+            if (contains == false)
+            {
+                return true;
+            }
+
+          
+        }
+
+        return false;
+    }
+}
