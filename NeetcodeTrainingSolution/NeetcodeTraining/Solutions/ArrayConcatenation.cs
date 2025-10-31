7ca81261e5ef1065ed8b6bfdc62e6eba33203c3c
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-10-31 09:44:19 +0530

    solution 1 for replace element with greatest on the right

---
+++ NeetcodeTrainingSolution/NeetcodeTraining/Solutions/ArrayConcatenation.cs
@@ -0,0 +1,17 @@
+﻿namespace NeetcodeTraining.Solutions;
+
+public class ArrayConcatenation
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        int[] ans = new int[nums.Length * 2];
+
+        int ansLength = ans.Length;
+        int numsLength = nums.Length;
+
+        for (int i = 0; i < ansLength; i++)
+        {
+            ans[i] = nums[i%numslength];
+        }
+    }
+}
